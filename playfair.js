1e7ad23c969010e4900818cf3f5ccd3b9cd34092
dcdholder <dcdholder@gmail.com>
2018-10-08 17:30:07 -0400

    Allows script to decode test case from film
    Key and cipher taken from scene, output is as follows:
    LABOULAYELADYWILLXLEADTOCIBOLATEMPLESOFGOLDX

--- playfair.js
+++ playfair.js
@@ -1,15 +1,50 @@
+//movie uses J substitution and left/up row/column decipher directions
+
 //let KNOWN_KEY_LENGTH  = 5;
 let MATRIX_SIZE       = 5;
 let UPPERCASE_LETTERS = String.fromCharCode(...Array(91).keys()).slice(65).split('');
 let ALL_WORDS         = require('./words.json');
+ALL_WORDS         = ['DEATH'];
 
-function createKeyMatrix(key) {
-  let keyChars  = key.split('');
+let OMITTED_LETTER           = 'J';
+let SUBSTITUTE_LETTER        = 'I';
+let DOUBLE_LETTER_SUBSTITUTE = 'X';
+
+function reduceKey(rawKey) {
+  let keyChars = rawKey.toUpperCase().split('');
+
+  //remove duplicate letters from the key
+  let filteredKeyChars = [];
+  for (let letter of keyChars) {
+    //handle letter subsitution in the key (wrong letter total if only performed on the "fill letters")
+    let newLetterAndNotOmittedLetter       = filteredKeyChars.indexOf(letter)===-1 && letter!=OMITTED_LETTER;
+    let omittedLetterWithSubstituteMissing = filteredKeyChars.indexOf(SUBSTITUTE_LETTER)===-1 && letter==OMITTED_LETTER;
+
+    //console.log(newLetterAndNotOmittedLetter, omittedLetterWithSubstituteMissing);
+
+
+    if (newLetterAndNotOmittedLetter || omittedLetterWithSubstituteMissing) {
+      if (letter!=OMITTED_LETTER) {
+        filteredKeyChars.push(letter);
+      } else {
+        filteredKeyChars.push(SUBSTITUTE_LETTER);
+      }
+    }
+  }
+
+  return filteredKeyChars;
+}
+
+function createKeyMatrix(rawKey) {
+  let keyChars  = reduceKey(rawKey);
   let keyMatrix = [];
 
+  //collect all unused letters in order
   let unusedAlphabetChars = UPPERCASE_LETTERS.filter((char) => {
-    return keyChars.indexOf(char) === -1;
+    return keyChars.indexOf(char) === -1 && char!=OMITTED_LETTER;
   });
+
+  //matrix first filled with reduced key letters, then all other letters in alphabet
   for (let j=0;j<MATRIX_SIZE;j++) {
     keyMatrix.push([]);
     for (let i=0;i<MATRIX_SIZE;i++) {
@@ -20,6 +55,8 @@ function createKeyMatrix(key) {
       }
     }
   }
+
+  console.log(keyMatrix);
 
   return keyMatrix;
 }
@@ -68,7 +105,7 @@ function englishnessScore(str) {
 
 //keys generated one-by-one to allow concurrent implementations
 function generatePossibleKey(i) {
-  return ALL_WORDS[i];
+  return ALL_WORDS[i].toUpperCase();
 }
 
 //TODO: reuse this to allow non-word keys - probably very slow
@@ -99,31 +136,37 @@ function base26Conversion(int) {
 */
 
 function solveCipher(cipherText,keyMatrix) {
-  let cipher                      = cipher.split(" ").join();
-  let cipherChars                 = cipherText.split('');
+  let cipherChars                 = cipherText.split(" ").join('').split('');
   let cipherCharsPairs            = [];
   let cipherCharsPairsCoordinates = [];
 
   //split ciphertext into character pairs
   for (let i=0;i<cipherChars.length;i+=2) {
     cipherCharsPairs.push([cipherChars[i],cipherChars[i+1]]);
-    cipherCharsPairsCoordinates.push([new Array(2)]);
+    cipherCharsPairsCoordinates.push([new Array(2),new Array(2)]);
   }
+
+  //console.log(cipherCharsPairs);
 
   //grab coordinates for the components of each character pair
   for (let k=0;k<cipherCharsPairs.length;k++) {
     for (let j=0;j<keyMatrix.length;j++) {
       for (let i=0;i<keyMatrix.length;i++) {
-        if (keyMatrix[j][i]==cipherChars[k][0]) {
+        if (keyMatrix[j][i]==cipherCharsPairs[k][0]) {
           cipherCharsPairsCoordinates[k][0] = [i,j];
-        } else if (keyMatrix[j][i]==cipherChars[k][1]) {
+        }
+
+        if (keyMatrix[j][i]==cipherCharsPairs[k][1]) {
           cipherCharsPairsCoordinates[k][1] = [i,j];
         }
       }
     }
   }
 
+  //console.log(cipherCharsPairsCoordinates);
+
   //decipher according to the 4 possible character pair scenarios (column/row/same/rectangle)
+  let outputChars = [];
   for (let i=0;i<cipherCharsPairsCoordinates.length;i++) {
     let aInitial = cipherCharsPairsCoordinates[i][0];
     let bInitial = cipherCharsPairsCoordinates[i][1];
@@ -134,50 +177,59 @@ function solveCipher(cipherText,keyMatrix) {
     let yAInitial = aInitial[1];
     let yBInitial = bInitial[1];
 
+    let xA, xB;
+    let yA, yB;
+
     if (xAInitial==xBInitial) { //check for columns
-      let xA = xAInitial;
-      let xB = xBInitial;
+      xA = xAInitial;
+      xB = xBInitial;
 
-      let yA = yAInitial + 1;
-      let yB = yBInitial + 1;
+      yA = yAInitial - 1;
+      yB = yBInitial - 1;
 
-      //wrap around if either final x coordinate is off the grid
-      if (xAInitial==keyMatrix.length) {
-        xA = 0;
+      //wrap around if either final y coordinate is off the grid
+      if (yA==-1) {
+        yA = keyMatrix.length-1;
       }
 
-      if (xBInitial==keyMatrix.length) {
-        xB = 0;
+      if (yB==-1) {
+        yB = keyMatrix.length-1;
       }
     } else if (yAInitial==yBInitial) { //check for rows
-      let xA = xAInitial + 1;
-      let xB = xBInitial + 1;
+      xA = xAInitial - 1;
+      xB = xBInitial - 1;
 
-      let yA = yAInitial;
-      let yB = yBInitial;
+      yA = yAInitial;
+      yB = yBInitial;
 
       //wrap around if either final x coordinate is off the grid
-      if (yAInitial==keyMatrix.length) {
-        yA = 0;
+      if (xA==-1) {
+        xA = keyMatrix.length-1;
       }
 
-      if (yBInitial==keyMatrix.length) {
-        yB = 0;
+      if (xB==-1) {
+        xB = keyMatrix.length-1;
       }
     } else { //otherwise, we have a rectangle
-      let xA = xBInitial; //switch x coordinates
-      let xB = xAInitial;
+      xA = xBInitial; //switch x coordinates
+      xB = xAInitial;
 
-      let yA = yAInitial; //do not switch y coordinates
-      let yB = yBInitial;
+      yA = yAInitial; //do not switch y coordinates
+      yB = yBInitial;
     }
 
-    outputCharPairs.push([keyMatrix[y0][x0],keyMatrix[y1][x1]]);
+    //console.log(xAInitial,yAInitial,xA,yA);
+    //console.log(xBInitial,yBInitial,xB,yB);
+
+    outputChars.push(keyMatrix[yA][xA]);
+    outputChars.push(keyMatrix[yB][xB]);
   }
 
   //glue the resulting letter pairs into a string and return
-  return outputCharPairs.join();
+  return outputChars.join('');
 }
+
+//TODO: add encryption capability
 
 let cipherText = "ME IK QO TX CQ TE ZX CO MW QC TE HN FB IK ME HA KR QC UN GI KM AV";
 console.log(bruteForceCipher(cipherText)[1]);
