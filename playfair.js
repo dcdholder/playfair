076cf5e8a36e8b01c622eb61620d7d64f1e7ad8e
dcdholder <dcdholder@gmail.com>
2018-10-08 17:58:24 -0400

    Removes brute-forcing code

--- playfair.js
+++ playfair.js
@@ -20,9 +20,6 @@ function reduceKey(rawKey) {
     let newLetterAndNotOmittedLetter       = filteredKeyChars.indexOf(letter)===-1 && letter!=OMITTED_LETTER;
     let omittedLetterWithSubstituteMissing = filteredKeyChars.indexOf(SUBSTITUTE_LETTER)===-1 && letter==OMITTED_LETTER;
 
-    //console.log(newLetterAndNotOmittedLetter, omittedLetterWithSubstituteMissing);
-
-
     if (newLetterAndNotOmittedLetter || omittedLetterWithSubstituteMissing) {
       if (letter!=OMITTED_LETTER) {
         filteredKeyChars.push(letter);
@@ -56,86 +53,11 @@ function createKeyMatrix(rawKey) {
     }
   }
 
-  console.log(keyMatrix);
-
   return keyMatrix;
 }
 
-//begin and end parameters used in a concurrent implementation
-//can be left blank to search entire space
-function bruteForceCipher(cipherText,begin,end) {
-  let bestKey;
-  let bestResults;
-
-  if (!begin) {
-    begin = 0;
-  }
-
-  if (!end) {
-    end = ALL_WORDS.length-1;
-    //end = Math.floor(Math.pow(26,KNOWN_KEY_LENGTH))-1;
-  }
-
-  for (let i=begin; i<=end; i++) {
-    let key            = generatePossibleKey(i);
-    let keyMatrix      = createKeyMatrix(key);
-    let attemptResults = solveCipher(cipherText,keyMatrix);
-
-    if (!bestResults || englishnessScore(attemptResults) > bestResults) {
-      bestKey     = key;
-      bestResults = attemptResults;
-    }
-  }
-
-  return [bestKey,bestResults];
-}
-
-function englishnessScore(str) {
-  let englishnessScore = 0;
-
-  let words = str.split(' ');
-  for (word of words) {
-    if (word in ALL_WORDS) {
-      englishnessScore+=1;
-    }
-  }
-
-  return englishnessScore;
-}
-
-//keys generated one-by-one to allow concurrent implementations
-function generatePossibleKey(i) {
-  return ALL_WORDS[i].toUpperCase();
-}
-
-//TODO: reuse this to allow non-word keys - probably very slow
-/*
-//generate single keys on demand to save on memory
-function generatePossibleKey(keyIndex) {
-  let keyChars = [];
-
-  let letterIndexes = base26Conversion(keyIndex);
-  for (let i=0;i<letterIndexes.length;i++) {
-    keyChars.push(UPPERCASE_LETTERS[letterIndexes[i]]);
-  }
-
-  return keyChars.join();
-}
-
-function base26Conversion(int) {
-  let tmpInt = int;
-
-  let base26Digits = [];
-  while (tmpInt>0) {
-    base26Digits.push(tmpInt%26);
-    tmpInt = Math.floor(tmpInt/26);
-  }
-
-  return tmpInt;
-}
-*/
-
-function solveCipher(cipherText,keyMatrix) {
+function solveCipher(cipherText,rawKey) {
+  let keyMatrix                   = createKeyMatrix(rawKey);
   let cipherChars                 = cipherText.split(" ").join('').split('');
   let cipherCharsPairs            = [];
   let cipherCharsPairsCoordinates = [];
@@ -145,8 +67,6 @@ function solveCipher(cipherText,keyMatrix) {
     cipherCharsPairs.push([cipherChars[i],cipherChars[i+1]]);
     cipherCharsPairsCoordinates.push([new Array(2),new Array(2)]);
   }
-
-  //console.log(cipherCharsPairs);
 
   //grab coordinates for the components of each character pair
   for (let k=0;k<cipherCharsPairs.length;k++) {
@@ -162,8 +82,6 @@ function solveCipher(cipherText,keyMatrix) {
       }
     }
   }
-
-  //console.log(cipherCharsPairsCoordinates);
 
   //decipher according to the 4 possible character pair scenarios (column/row/same/rectangle)
   let outputChars = [];
@@ -218,9 +136,6 @@ function solveCipher(cipherText,keyMatrix) {
       yB = yBInitial;
     }
 
-    //console.log(xAInitial,yAInitial,xA,yA);
-    //console.log(xBInitial,yBInitial,xB,yB);
-
     outputChars.push(keyMatrix[yA][xA]);
     outputChars.push(keyMatrix[yB][xB]);
   }
@@ -230,6 +145,6 @@ function solveCipher(cipherText,keyMatrix) {
 }
 
 //TODO: add encryption capability
-
+let rawKey     = "DEATH";
 let cipherText = "ME IK QO TX CQ TE ZX CO MW QC TE HN FB IK ME HA KR QC UN GI KM AV";
-console.log(bruteForceCipher(cipherText)[1]);
+console.log(solveCipher(cipherText,rawKey));
